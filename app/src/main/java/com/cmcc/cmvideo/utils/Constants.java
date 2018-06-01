af5f1b47ab51bc53df4aefcf0f42de168c61d7ee
yuyawang <19907146109@189.cn>
2018-06-01 11:13:07 +0800

    增加item类型

--- app/src/main/java/com/cmcc/cmvideo/utils/Constants.java
+++ app/src/main/java/com/cmcc/cmvideo/utils/Constants.java
@@ -6,11 +6,14 @@ package com.cmcc.cmvideo.utils;
  */
 
 public class Constants {
-    public final static int MESSAGE_TYPE_NORMAL = 0;
-    public final static int MESSAGE_TYPE_OTHER = 1;
 
-    public final static String MESSAGE_FROM_AI = "message_from_ai";
-    public final static String MESSAGE_FROM_USER = "message_from_user";
-    public final static String MESSAGE_FROM_OTHER = "message_from_other";
+    public final static String MESSAGE_FROM_AI = "message_from_ai";//消息来自AI
+    public final static String MESSAGE_FROM_USER = "message_from_user";//消息来自用户
 
+    public final static int MESSAGE_TYPE_NORMAL = 0;//聊天类消息
+    public final static int MESSAGE_TYPE_CAN_ASK_AI = 1;//可以这样问AI
+    public final static int MESSAGE_TYPE_APPOINTMENT = 2;//AI预约电影成功的类型
+    public final static int MESSAGE_TYPE_EVERYONE_IS_WATCHING = 3;//大家都在看
+    public final static int MESSAGE_TYPE_GUESS_WHAT_YOU_LIKE = 4;//猜你喜欢
+    public final static int MESSAGE_TYPE_THE_LATEST_VIDEO = 5;//最新影讯
 }
