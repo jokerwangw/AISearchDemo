c58ddc30de81c3207b18b2d6f140c83c69ad8b3e
yuyawang <19907146109@189.cn>
2018-06-02 14:51:35 +0800

    ADD TYPE

--- app/src/main/java/com/cmcc/cmvideo/utils/Constants.java
+++ app/src/main/java/com/cmcc/cmvideo/utils/Constants.java
@@ -14,6 +14,9 @@ public class Constants {
     public final static int MESSAGE_TYPE_CAN_ASK_AI = 1;//可以这样问AI
     public final static int MESSAGE_TYPE_APPOINTMENT = 2;//AI预约电影成功的类型
     public final static int MESSAGE_TYPE_EVERYONE_IS_WATCHING = 3;//大家都在看
+    public final static int MESSAGE_TYPE_I_WANT_TO_SEE = 8;//我想看XXXX
     public final static int MESSAGE_TYPE_GUESS_WHAT_YOU_LIKE = 4;//猜你喜欢
+    public final static int MESSAGE_TYPE_GUESS_WHAT_YOU_LIKE_LIST_HORIZONTAL = 6;//猜你喜欢_列表横向展示
+    public final static int MESSAGE_TYPE_GUESS_WHAT_YOU_LIKE_LIST_VERTICAL = 7;//猜你喜欢_列表垂直展示
     public final static int MESSAGE_TYPE_THE_LATEST_VIDEO = 5;//最新影讯
 }
